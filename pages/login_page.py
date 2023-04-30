6672b9670d9b0c9aeb4ebf3471c3797878afb04d
Eugene Reydel <evgeny.reydel@yandex.ru>
2023-04-30 19:23:41 +0700

    added transition to login page in test_main_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -10,17 +10,17 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
+        # проверка корректности запущенной страницы логина
         assert "login" in self.browser.current_url
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
+        # проверка наличия формы авторизации
         assert self.is_element_present(
             *LoginPageLocators.LOGIN_FORM
         ), "Login form is not presented"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
+        # проверка наличия формы регистрации
         assert self.is_element_present(
             *LoginPageLocators.REGISTER_FORM
         ), "Register form is not presented"
