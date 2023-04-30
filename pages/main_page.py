0b535b1ff662edbac4fcb81b58231f4f187586d2
Eugene Reydel <evgeny.reydel@yandex.ru>
2023-04-30 17:40:30 +0700

    added exception handler for 'Page Object'

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,9 +1,14 @@
 from selenium.webdriver.common.by import By
 
-from base_page import BasePage
+from pages.base_page import BasePage
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(
+            locator=By.CSS_SELECTOR, value="#login_link_invalid"
+        ), "Login link is not presented"
